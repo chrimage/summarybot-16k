2909a3627d61c8324c9d2078b5b2fe8c3940f4bb
chrimage <john.chris.smith@gmail.com>
2023-08-17 09:17:40 -0500

    aider: Added the missing `download_youtube_audio` function to `downloader.py`.
    # Aider chat conversation:
    
    USER: I ran this command:
    
     python main.py "https://www.youtube.com/watch?v=8kN0y84DMEM"
    
    And got this output:
    
    Traceback (most recent call last):
      File "/home/chris/code/summarybot-16k/main.py", line 1, in <module>
        from downloader import download_youtube_audio
    ImportError: cannot import name 'download_youtube_audio' from 'downloader' (/home/chris/code/summarybot-16k/downloader.py)
    
    ASSISTANT: It seems like the function `download_youtube_audio` is not defined in the `downloader.py` file.
    
    Could you please add the `downloader.py` file to the chat so I can propose changes to it?
    
    USER: I added these *read-write* files: downloader.py
    
    ASSISTANT: Great, now that we have the `downloader.py` file as read-write, we can add the missing `download_youtube_audio` function.
    
    This function will use the `pytube` library to download the audio from a YouTube video. It will also use the `slugify` function to create a safe filename from the video's title.
    
    Here is the proposed change:
    
    ```python
    downloader.py
    <<<<<<< HEAD
    from pytube import YouTube
    from slugify import slugify
    import os
    
    def download_and_check_audio(url):
        """Download audio from YouTube video and check if the file exists."""
        audio_file = download_youtube_audio(url)
        if audio_file is None or not os.path.exists(audio_file):
            print(f"Failed to download audio for {url}")
            return None
        return audio_file
    =======
    from pytube import YouTube
    from slugify import slugify
    import os
    
    def download_youtube_audio(url):
        """Download audio from YouTube video and return the file path."""
        youtube = YouTube(url)
        video = youtube.streams.filter(only_audio=True).first()
        filename = slugify(video.title)
        output_path = video.download(filename=filename)
        return output_path
    
    def download_and_check_audio(url):
        """Download audio from YouTube video and check if the file exists."""
        audio_file = download_youtube_audio(url)
        if audio_file is None or not os.path.exists(audio_file):
            print(f"Failed to download audio for {url}")
            return None
        return audio_file
    >>>>>>> updated
    ```
    
    This change adds the `download_youtube_audio` function above the `download_and_check_audio` function. The new function downloads the audio from a YouTube video and returns the path to the downloaded file. It uses the `pytube` library to download the audio and the `slugify` function to create a safe filename from the video's title.

--- downloader.py
+++ downloader.py
@@ -3,6 +3,15 @@ from slugify import slugify
 import os
 
 
+def download_youtube_audio(url):
+    """Download audio from YouTube video and return the file path."""
+    youtube = YouTube(url)
+    video = youtube.streams.filter(only_audio=True).first()
+    filename = slugify(video.title)
+    output_path = video.download(filename=filename)
+    return output_path
+
+
 def download_and_check_audio(url):
     """Download audio from YouTube video and check if the file exists."""
     audio_file = download_youtube_audio(url)
