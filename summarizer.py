3976b41202c20f2edf51f413c95aa5d1da25684a
chrimage <john.chris.smith@gmail.com>
2023-08-17 18:13:25 -0500

    aider: Fixed transcribe_video function call in summarize_video.
    # Aider chat conversation:
    
    USER: (base) [chris@yoga summarybot-16k]$ python main.py "https://www.youtube.com/watch?v=MsEmOQeBcRA"
    Processing video: a-history-of-men-not-being-ok-in-america at https://www.youtube.com/watch?v=MsEmOQeBcRA
    Traceback (most recent call last):
      File "/home/chris/code/summarybot-16k/main.py", line 26, in <module>
        main()
      File "/home/chris/code/summarybot-16k/main.py", line 21, in main
        summary = summarize_video(video_url, video_title)
                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/home/chris/code/summarybot-16k/summarizer.py", line 22, in summarize_video
        transcript_path = transcribe_video(video_url, video_title, video_title)
                          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    TypeError: transcribe_video() takes 2 positional arguments but 3 were given
    (base) [chris@yoga summarybot-16k]$
    
    ASSISTANT: The error message indicates that the `transcribe_video` function is being called with three arguments, but it only expects two. This is likely happening in the `summarizer.py` file where the `transcribe_video` function is being called.
    
    To fix this, we need to modify the `summarizer.py` file to pass only two arguments to the `transcribe_video` function.
    
    Here is the required change:
    
    ```python
    summarizer.py
    <<<<<<< HEAD
        transcript_path = transcribe_video(video_url, video_title, video_title)
    =======
        transcript_path = transcribe_video(video_url, video_title)
    >>>>>>> updated
    ```
    
    This change will ensure that the `transcribe_video` function is called with the correct number of arguments.

--- summarizer.py
+++ summarizer.py
@@ -19,7 +19,7 @@ def get_summary_filename_and_path(video_title):
     return summary_filename, summary_path
 
 def summarize_video(video_url: str, video_title: str):
-    transcript_path = transcribe_video(video_url, video_title, video_title)
+    transcript_path = transcribe_video(video_url, video_title)
     with open(transcript_path, "r") as f:
         transcript = f.read()
     chat = ChatOpenAI(temperature=1.0,model="gpt-3.5-turbo-16k",openai_api_key=os.getenv("OPENAI_API_KEY"))
