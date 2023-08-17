20a2a3478a5cde6a017bfb4a5f60ab1e08ddd069
chrimage <john.chris.smith@gmail.com>
2023-08-17 07:44:43 -0500

    aider: Updated `summarizer.py` to load the system message content from a text file.
    # Aider chat conversation:
    
    USER: update `summarizer.py` to load the system message content from a text file
    
    ASSISTANT: To implement this change, we need to modify the `summarizer.py` file.
    
    The steps are as follows:
    
    1. Import the `os` module if it's not already imported.
    2. Define a variable for the path to the text file containing the system message content.
    3. Use `os.path.exists` to check if the file exists.
    4. If the file exists, open it and read the content into a variable.
    5. Replace the hardcoded system message content with the content read from the file.
    
    Here is the *edit block* for the changes:
    
    ```python
    summarizer.py
    <<<<<<< HEAD
    from dotenv import load_dotenv
    load_dotenv()
    
    def summarize_video(video_url: str):
        transcript_path = transcribe_video(video_url)
        with open(transcript_path, "r") as f:
            transcript = f.read()
        chat = ChatOpenAI(temperature=1.0,model="gpt-3.5-turbo-16k",openai_api_key=os.getenv("OPENAI_API_KEY"))
        messages = [
            SystemMessage(
                content="You are a YouTube video summarizer. You will be provided with a transcript of the video. Please reply with a detailed summary of the video."
                ),
            HumanMessage(content=transcript)
        ]
    =======
    from dotenv import load_dotenv
    load_dotenv()
    
    def summarize_video(video_url: str):
        transcript_path = transcribe_video(video_url)
        with open(transcript_path, "r") as f:
            transcript = f.read()
        chat = ChatOpenAI(temperature=1.0,model="gpt-3.5-turbo-16k",openai_api_key=os.getenv("OPENAI_API_KEY"))
    
        system_message_file = "system_message.txt"
        if os.path.exists(system_message_file):
            with open(system_message_file, "r") as f:
                system_message_content = f.read()
        else:
            system_message_content = "You are a YouTube video summarizer. You will be provided with a transcript of the video. Please reply with a detailed summary of the video."
    
        messages = [
            SystemMessage(content=system_message_content),
            HumanMessage(content=transcript)
        ]
    >>>>>>> updated
    ```
    This change will load the system message content from a text file named `system_message.txt` if it exists. If the file does not exist, it will use the hardcoded message.

--- summarizer.py
+++ summarizer.py
@@ -11,16 +11,21 @@ from langchain.schema import AIMessage, HumanMessage, SystemMessage
 from dotenv import load_dotenv
 load_dotenv()
 
-
 def summarize_video(video_url: str):
     transcript_path = transcribe_video(video_url)
     with open(transcript_path, "r") as f:
         transcript = f.read()
     chat = ChatOpenAI(temperature=1.0,model="gpt-3.5-turbo-16k",openai_api_key=os.getenv("OPENAI_API_KEY"))
+    
+    system_message_file = "system_message.txt"
+    if os.path.exists(system_message_file):
+        with open(system_message_file, "r") as f:
+            system_message_content = f.read()
+    else:
+        system_message_content = "You are a YouTube video summarizer. You will be provided with a transcript of the video. Please reply with a detailed summary of the video."
+    
     messages = [
-        SystemMessage(
-            content="You are a YouTube video summarizer. You will be provided with a transcript of the video. Please reply with a detailed summary of the video."
-            ),
+        SystemMessage(content=system_message_content),
         HumanMessage(content=transcript)
     ]
     response = chat(messages)
