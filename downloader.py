ae6cd583337e156069c1d6326f0c6b2393810418
chrimage <john.chris.smith@gmail.com>
2023-08-18 09:35:16 -0500

    aider: Fixed issue with downloader creating a folder with the name of the audio file instead of setting the output path correctly.
    # Aider chat conversation:
    
    USER: this code base is sort of a mess. it was working once, but too many attempted improvements have caused issues. Please help
    
    ASSISTANT: Of course, I'd be happy to help. Could you please provide more specific details about the issues you're experiencing? For example, are there any error messages? What functionality isn't working as expected? The more information you can provide, the better I can assist you.
    
    USER: I added these *read-write* files: downloader.py
    
    ASSISTANT: Great! Now that we have the `downloader.py` file as *read-write*, we can make changes to it. However, to provide the most effective assistance, I still need more specific details about the issues you're experiencing. Could you please provide more information? For example, are there any error messages? What functionality isn't working as expected?
    
    USER: the downloader is creating a folder with the name of the audio file, that's wrong
    
    ASSISTANT: I see. The issue seems to be with the `download_youtube_audio` function in the `downloader.py` file. The `output_path` is being set incorrectly. It should be a directory, but it's being set to the filename.
    
    Here's how we can fix it:
    
    ```python
    downloader.py
    <<<<<<< HEAD
        filename = slugify(video_title) + '.webm'
        output_path = os.path.join(os.getcwd(), 'audio', filename)
        from utils import ensure_directory_exists
        ensure_directory_exists('audio')
        if os.path.exists(output_path):
            print(f"Audio file already exists at: {output_path}")
        else:
            print(f"Downloading audio from: {url}")
            output_path = video.download(filename=filename, output_path=output_path)
            print(f"Audio downloaded at: {output_path}")
        return output_path
    =======
        filename = slugify(video_title) + '.webm'
        output_directory = os.path.join(os.getcwd(), 'audio')
        from utils import ensure_directory_exists
        ensure_directory_exists(output_directory)
        output_path = os.path.join(output_directory, filename)
        if os.path.exists(output_path):
            print(f"Audio file already exists at: {output_path}")
        else:
            print(f"Downloading audio from: {url}")
            video.download(filename=filename, output_path=output_directory)
            print(f"Audio downloaded at: {output_path}")
        return output_path
    >>>>>>> updated
    ```
    
    In the updated code, we first ensure that the 'audio' directory exists. Then, we join the directory path with the filename to get the full path of the audio file. When downloading the audio, we specify the output directory, not the full file path.

--- downloader.py
+++ downloader.py
@@ -9,14 +9,15 @@ def download_youtube_audio(url, video_title):
     youtube = YouTube(url)
     video = youtube.streams.filter(only_audio=True).first()
     filename = slugify(video_title) + '.webm'
-    output_path = os.path.join(os.getcwd(), 'audio', filename)
+    output_directory = os.path.join(os.getcwd(), 'audio')
     from utils import ensure_directory_exists
-    ensure_directory_exists('audio')
+    ensure_directory_exists(output_directory)
+    output_path = os.path.join(output_directory, filename)
     if os.path.exists(output_path):
         print(f"Audio file already exists at: {output_path}")
     else:
         print(f"Downloading audio from: {url}")
-        output_path = video.download(filename=filename, output_path=output_path)
+        video.download(filename=filename, output_path=output_directory)
         print(f"Audio downloaded at: {output_path}")
     return output_path
 
